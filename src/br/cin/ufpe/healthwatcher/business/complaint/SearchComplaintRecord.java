c486337e0ea0cca016a3d5f1192c300db39d9405
Fagner Fernandes Candido da Silva <ffcandido@trf5.jus.br>
2015-11-06 16:15:43 -0300

    continuacao do ajueste do searchComplaint.

--- src/br/cin/ufpe/healthwatcher/business/complaint/SearchComplaintRecord.java
+++ src/br/cin/ufpe/healthwatcher/business/complaint/SearchComplaintRecord.java
@@ -5,11 +5,9 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.context.Flash;
 
 import lib.exceptions.ObjectNotFoundException;
 import lib.exceptions.PersistenceMechanismException;
@@ -17,9 +15,7 @@ import lib.exceptions.RepositoryException;
 import lib.exceptions.TransactionException;
 import lib.util.IteratorDsk;
 import br.cin.ufpe.healthwatcher.business.HealthWatcherFacade;
-import br.cin.ufpe.healthwatcher.model.complaint.AnimalComplaint;
 import br.cin.ufpe.healthwatcher.model.complaint.Complaint;
-import br.cin.ufpe.healthwatcher.model.complaint.SpecialComplaint;
 
 @ManagedBean
 @RequestScoped
@@ -27,35 +23,23 @@ public class SearchComplaintRecord implements Serializable {
 
 	private static final long serialVersionUID = -6887424307646650506L;
 	
-	private Complaint complaint;
+	private int complaintCode;
+	
+	public int getComplaintCode() {
+		return complaintCode;
+	}
+
+	public void setComplaintCode(int complaintCode) {
+		this.complaintCode = complaintCode;
+	}
+
 	private List<Complaint> complaints;
 	private boolean foodComplaintFlag = false;
 	private boolean animalComplaintFlag = false;
 	private boolean specialComplaintFlag = false;
 	private boolean noDataFound;
-	private String complaintKind;
-	HealthWatcherFacade facade;
+	private HealthWatcherFacade facade;
 	
-	@PostConstruct
-	private void init(){
-		FacesContext facesContext = FacesContext.getCurrentInstance();
-		if(facesContext!=null){
-			Flash flash = facesContext.getExternalContext().getFlash();
-			String codigo = (String) flash.get("complaint");
-			if(codigo!=null){
-				try {
-					if(facade==null){
-						facade = HealthWatcherFacade.getInstance();
-					}
-					this.complaint = facade.getfCid().searchComplaint(Integer.parseInt(codigo));
-				} catch (NumberFormatException | RepositoryException | PersistenceMechanismException | IOException
-						| ObjectNotFoundException | TransactionException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-
 	public boolean isFoodComplaintFlag() {
 		return foodComplaintFlag;
 	}
@@ -91,14 +75,6 @@ public class SearchComplaintRecord implements Serializable {
 		this.noDataFound = noDataFound;
 	}
 
-	public Complaint getComplaint() {
-		return complaint;
-	}
-
-	public void setComplaint(Complaint complaint) {
-		this.complaint = complaint;
-	}
-
 	public List<Complaint> getComplaints() {
 		this.complaints = new ArrayList<Complaint>();
 		try{
@@ -120,37 +96,21 @@ public class SearchComplaintRecord implements Serializable {
 		this.complaints = complaints;
 	}
 	
-	public String searchComplaint(){
+	public String search(){
 		FacesContext facesContext = FacesContext.getCurrentInstance();
-		facesContext.getExternalContext().getFlash().put("complaint", complaint.getCodigo());
-		facesContext.getExternalContext().getFlash().put("complaintKind", complaintKind);
-		return "updateSearchComplaint?faces-redirect=true";
-	}
-	
-	public String updateComplaint(){
-		try{
+		Complaint complaint;
+		try {
 			if(facade==null){
 				facade = HealthWatcherFacade.getInstance();
 			}
-			facade.getfCid().getComplaintRecord().update(this.complaint);
-			init();
-			return "updateSearchComplaintData?faces-redirect=true";
-		} catch (Exception e) {
-			return "";
+			complaint = facade.getfCid().searchComplaint(complaintCode);
+			facesContext.getExternalContext().getFlash().put("complaint", complaint.getCodigo());
+			return "updateSearchComplaint?faces-redirect=true";			
+		} catch (RepositoryException | ObjectNotFoundException | IOException | PersistenceMechanismException
+				| TransactionException e) {
+			e.printStackTrace();
 		}
-	}	
-
-	public String getComplaintKind() {
-		if(this.complaint!=null){
-			if(this.complaint instanceof AnimalComplaint){
-				complaintKind = "Animal complaint";
-			} else if(this.complaint instanceof SpecialComplaint){
-				complaintKind = "Special complaint";
-			} else {
-				complaintKind = "Food complaint";
-			}
-		}
-		return complaintKind;
+		return "";
 	}
 
 }
